1cd76fa712e585a77b243abfeef706181ac9fa46
Calle <carl@lindstedt.net>
2020-11-17 12:51:39 +0100

    skapat nya färger för spelet (Gameboy)

--- Novemberprojekt/Program.cs
+++ Novemberprojekt/Program.cs
@@ -7,6 +7,11 @@ namespace Novemberprojekt
     {
         static void Main(string[] args)
         {
+            Color lightestGreen = new Color(155, 188, 15, 255);
+            Color lightGreen = new Color(139, 172, 15, 255);
+            Color darkGreen = new Color (48, 98, 48, 255);
+            Color darkestGreen = new Color (15, 56, 15, 255);
+
             Raylib.InitWindow(1000, 800, "Slime Shooter");
 
             Raylib.SetTargetFPS(60); //Sätter fps till 60.
@@ -15,9 +20,9 @@ namespace Novemberprojekt
             {
             Raylib.BeginDrawing();
       
-            Raylib.ClearBackground(Color.GREEN);
+            Raylib.ClearBackground(lightGreen);
             
-            Raylib.DrawRectangle(100, 100, 30, 60, Color.DARKGREEN);
+            Raylib.DrawRectangle(100, 100, 30, 60, darkGreen);
             
             Raylib.EndDrawing();
             }
