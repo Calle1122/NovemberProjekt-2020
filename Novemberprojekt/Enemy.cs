6ebcb7ce06bf1c98db1c037310b600bcc450162c
Calle <carl@lindstedt.net>
2020-11-18 10:48:17 +0100

    börjat skriva logik för enemy

--- Novemberprojekt/Enemy.cs
+++ Novemberprojekt/Enemy.cs
@@ -7,11 +7,25 @@ namespace Novemberprojekt
     public class Enemy
     {
         Random generator = new Random();
+        int Score = 0;
+        int HiScore;
+
+        public static List<Enemy> enemies = new List<Enemy>();
 
         Color darkestGreen = new Color (15, 56, 15, 255);
 
         public Enemy(){
+            int enemyType = generator.Next(1,3);
 
+            if(enemyType == 1){
+                //Gör en liten snabb enemy
+            }
+
+            else {
+                //Gör en stor långsam enemy
+            }
+            
+            enemies.Add(this);
         }
     }
 }
