d16345643b93530d2b728a81d1eccab8c0a38fee
Calle <carl@lindstedt.net>
2020-11-18 10:39:52 +0100

    skapat Enemy klassen

---
+++ Novemberprojekt/Enemy.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using Raylib_cs;
+
+namespace Novemberprojekt
+{
+    public class Enemy
+    {
+        Random generator = new Random();
+
+        Color darkestGreen = new Color (15, 56, 15, 255);
+
+        public Enemy(){
+
+        }
+    }
+}
